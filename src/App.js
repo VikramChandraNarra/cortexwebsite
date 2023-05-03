498ce70756bbd7005b6d2d040a1de2a677967b46
Pranav-Bha-tt <123838822+Pranav-Bha-tt@users.noreply.github.com>
2023-05-03 16:43:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import scansImage from './images/Scans 1.png';
 import NavBar from './components/NavBar';
 import MainPageStats from './components/Main-Page-Stats';
 import BottomPage from './components/bottom_page/BottomPage';
+import Bowser from 'bowser';
 import React, { useEffect } from 'react';
 import { db } from './authentication/Authentication';
 import { doc, setDoc, updateDoc, increment } from "firebase/firestore";
 
 function App() {
+  const browserFeatures = { "browser": ["name", "version"], "os": ["name", "version", "versionName"], "platform": ["type", "vendor", "model"], "engine": ["name", "version"] };
   useEffect(() => {
     const updateVisitorsCount = async () => {
       await updateDoc(doc(db, "visitors", "visitors"), {
@@ -39,18 +41,34 @@ function App() {
       }
 
       const currentDate = formatDate(new Date());
+      const browserInfo = getBrowserInfo();
 
       const documentData = {
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA",
+        time: currentDate,
+        referrer: `${document.referrer ? document.referrer : 'User came directly to the Cortex Website'}`
       };
 
       if (latitude !== '' && longitude !== '') {
         documentData.latitude = latitude;
         documentData.longitude = longitude;
       }
-
+      if (browserInfo) {
+        for (const key in browserFeatures) {
+          if (browserFeatures.hasOwnProperty(key)) {
+            const featureList = browserFeatures[key];
+            for (const feature of featureList) {
+              console.log(`Key: ${key}, Feature: ${feature}`);
+              const propertyName = key + feature.charAt(0).toUpperCase() + feature.slice(1);
+              if (typeof browserInfo[key] !== 'undefined' && typeof browserInfo[key][feature] !== 'undefined') {
+                documentData[propertyName] = browserInfo[key][feature];
+              }
+              else {
+                documentData[propertyName] = '';
+              }
+            }
+          }
+        }
+      }
       try {
         await setDoc(doc(db, "visitors_details", currentDate), documentData);
       } catch (error) {
@@ -66,6 +84,10 @@ function App() {
       const seconds = String(date.getSeconds()).padStart(2, '0');
 
       return `${year}${month}${day}-${hours}:${minutes}:${seconds}`;
+    };
+    const getBrowserInfo = () => {
+      const parser = Bowser.getParser(window.navigator.userAgent);
+      return parser.getResult();
     };
     updateVisitorsCount();
   }, []);
