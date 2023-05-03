a0fa9ea39862f4ea690b8bd51af30d15a94d2540
Pranav-Bha-tt <123838822+Pranav-Bha-tt@users.noreply.github.com>
2023-05-03 07:27:10 -0400

    Add bottom_page folder

---
+++ src/components/bottom_page/BottomPage.js
@@ -0,0 +1,37 @@
+import styles from './BottomPage.module.css';
+import scansImage2 from '../../images/Scans 2.png';
+import cortexImage from '../../images/Cerebral Cortex.png';
+
+function BottomPage() {
+    return (
+        <div className={styles['Bottom-Page']}>
+            <div className={styles['Bottom-Page-Upper']}>
+                <div className={styles['Bottom-Page-Upper-Left']}>
+                    <p className={styles['Sub-Top-Title']}>Cognitive Fitness Level</p>
+                    <p className={styles['Bottom-Page-Upper-Title']}>Understand yourself & others</p>
+                    <p className={styles['Sub-Title']}>In our free assessment you’ll learn what really drives, inspires, worries different personality types, helping you build more meaning ful relationships</p>
+                    <p className={styles['Explore-Link']}>EXPLORE COGNITIVE FITNESS</p>
+                </div>
+                <img className={styles['Scans-Image-Two']} src={scansImage2} alt="Scans"></img>
+            </div>
+            <div className={styles['Bottom-Page-Lower']}>
+                <p className={styles['Bottom-Page-Lower-Title']}>Curious how accurate we are about you?</p>
+                <button className={styles['Bottom-Page-Button']}>Take the Test</button>
+                <div className={styles['Lower-Lower']}>
+                    <div className={styles['Lower-Left']}>
+                        <img className={styles['Lower-Left-Cortex']} src={cortexImage}></img><p className={styles['Lower-Left-Cortex-Name']}>CORTEX</p><p class={styles['Lower-Left-Copyrights']}>Copyright © Event-u All rights reserved</p>
+                    </div>
+                    <div className={styles['Lower-Right']}>
+                        <p className={styles['Option']}>Features</p>
+                        <p className={styles['Option']}>Features</p>
+                        <p className={styles['Option']}>Features</p>
+                        <p className={styles['Option']}>Features</p>
+                        <button className={styles['Comezar']}>Comenzar</button>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default BottomPage;
