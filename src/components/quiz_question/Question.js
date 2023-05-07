4a027d5d17c87b9944c1d285b2fb4184e1de6a3e
Pranav-Bha-tt <123838822+Pranav-Bha-tt@users.noreply.github.com>
2023-05-07 02:13:06 -0400

    Update Question.js

--- src/components/quiz_question/Question.js
+++ src/components/quiz_question/Question.js
@@ -2,23 +2,54 @@ import React, { useState } from 'react'
 import styles from './quiz.module.css'
 
 export const Question = (prop) => {
-    const [clicked, isClicked] = useState(-1) //Means nothing has been clicked yet
+  const [clicked, isClicked] = useState(-1)
+  const onClickHandler = (index) => {
+    if (clicked === index) {
+      isClicked(-1);
+    }
+    else {
+      prop.responseHandler(prop.identifier, index);
+      isClicked(index);
+    }
+  };
 
-    
+
   return (
     <div className={styles['question']}>
-        <h1 className={styles['heading_question']}>{prop.prompt}</h1>
-        <div className={styles['poll']}>
-            <p className={styles['agree_text']}>Agree</p>
-            <button onClick={() => isClicked(0)} className={styles['green1']}></button>
-            <button onClick={() => isClicked(1)} className={styles['green2']}></button>
-            <button onClick={() => isClicked(2)} className={styles['green3']}></button>
-            <button onClick={() => isClicked(3)} className={styles['gray']}></button>
-            <button onClick={() => isClicked(4)} className={styles['purple3']}></button>
-            <button onClick={() => isClicked(5)} className={styles['purple2']}></button>
-            <button onClick={() => isClicked(6)} className={styles['purple1']}></button>
-            <p className={styles['disagree_text']}>Disagree</p>
-        </div>
+      <h1 className={styles['heading_question']}>{prop.prompt}</h1>
+      <div className={styles['poll']}>
+        <p className={styles['agree_text']}>Agree</p>
+        <button
+          onClick={() => onClickHandler(0)}
+          className={`${styles['green1']} ${clicked === 0 ? `${styles['class0']}` : ''}`}
+        ></button>
+        <button
+          onClick={() => onClickHandler(1)}
+          className={`${styles['green2']} ${clicked === 1 ? `${styles['class1']}` : ''}`}
+        ></button>
+        <button
+          onClick={() => onClickHandler(2)}
+          className={`${styles['green3']} ${clicked === 2 ? `${styles['class2']}` : ''}`}
+        ></button>
+        <button
+          onClick={() => onClickHandler(3)}
+          className={`${styles['gray']} ${clicked === 3 ? `${styles['class3']}` : ''}`}
+        ></button>
+        <button
+          onClick={() => onClickHandler(4)}
+          className={`${styles['purple3']} ${clicked === 4 ? `${styles['class4']}` : ''}`}
+        ></button>
+        <button
+          onClick={() => onClickHandler(5)}
+          className={`${styles['purple2']} ${clicked === 5 ? `${styles['class5']}` : ''}`}
+        ></button>
+        <button
+          onClick={() => onClickHandler(6)}
+          className={`${styles['purple1']} ${clicked === 6 ? `${styles['class6']}` : ''}`}
+        ></button>
+
+        <p className={styles['disagree_text']}>Disagree</p>
+      </div>
     </div>
   )
 }
